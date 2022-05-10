42480f542429c1f013b40e599e42645cc2e7d5a6
Jack Klamer <jfklamer@gmail.com>
2022-05-09 22:53:35 -0500

    message sender

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,17 +1,28 @@
-use apache_avro::{AvroSchema};
+use apache_avro::{AvroSchema, SpecificSingleObjectWriter};
 use apache_avro::schema::derive::AvroSchemaComponent;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, AvroSchema)]
-pub struct AppendEntriesRequest<T> 
+pub struct AppendEntriesRequest<Command> 
 where
-    T: AvroSchemaComponent
+    Command: AvroSchemaComponent + Sync + Send
 {
     term : i64,
     leader_id: String,
     prev_log_index: i64,
     prev_log_term: i64,
-    entries: Vec<T>
+    entries: Vec<LogEntry<Command>>,
+    leader_commit: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, AvroSchema)]
+pub struct LogEntry<Command>
+where
+    Command : AvroSchemaComponent + Sync + Send
+{
+    term: i64,
+    log_idex: i64,
+    command: Command
 }
 
 #[derive(Debug, Serialize, Deserialize, AvroSchema)]
@@ -34,6 +45,51 @@ pub struct RequestVoteResult {
     vote_granted: bool
 }
 
+pub enum Message<Command> 
+where
+    Command: AvroSchemaComponent + Sync + Send
+{
+    AEReq(AppendEntriesRequest<Command>),
+    AERes(AppendEntriesResult),
+    RVReq(RequestVoteRequest),
+    RVRes(RequestVoteResult),
+}
+
+pub struct MessageSender<Command> 
+where 
+    Command: AvroSchemaComponent + Sync + Send
+{
+    append_entry_request_sender: SpecificSingleObjectWriter<AppendEntriesRequest<Command>>,
+    append_entry_result_sender: SpecificSingleObjectWriter<AppendEntriesResult>,
+    request_vote_request_sender: SpecificSingleObjectWriter<RequestVoteRequest>,
+    request_vote_result_sender: SpecificSingleObjectWriter<RequestVoteResult>
+}
+
+impl MessageSender<Command> {
+    pub fn new() -> MessageSender<Command> {
+        MessageSender::<Command> {
+            append_entry_request_sender: SpecificSingleObjectWriter::<AppendEntriesRequest::<Command>>::new(),
+            append_entry_result_sender: SpecificSingleObjectWriter::<AppendEntriesResult>::new(),
+            request_vote_request_sender: SpecificSingleObjectWriter::<RequestVoteRequest>::new(),
+            request_vote_result_sender: SpecificSingleObjectWriter::<RequestVoteResult>::new()
+        }
+    }
+
+    pub fn send(&self, m: Message<Command>) -> Result<u64, apache_avro::Error> {
+
+    }
+}
+
+
+
+
+}
+where
+    W: Write,
+    Command: AvroSchemaComponent + Sync + Send
+{
+
+}
 mod tests {
     use super::*;
 }
